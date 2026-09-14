6ff9f7045fbc1d9c4bb33b54ef1c4316d83d275c
Pavel Kalugin <pavel@pavelthebest.me>
2026-09-14 17:05:11 +0300

    add devshell

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Simple C++ / CMake development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+    in
+    {
+      devShells = nixpkgs.lib.genAttrs systems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          llvm = pkgs.llvmPackages_22;
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.git
+              pkgs.cmake
+              pkgs.ninja
+              llvm.clang
+              llvm.clang-tools
+            ];
+          };
+        });
+    };
+}
