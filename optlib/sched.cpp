7b272177a38a09260bdebabdea7d3a5f36a337da
Pavel Kalugin <pavel@pavelthebest.me>
2026-09-16 18:02:22 +0300

    implement linear and reciprocal schedulers

--- optlib/sched.cpp
+++ optlib/sched.cpp
@@ -2,18 +2,27 @@
 
 #include <cmath>
 
-ExpDecayScheduler::ExpDecayScheduler(long double start, long double step,
-                                     long double min)
-    : temp_(start), step_(step), min_(min) {}
+namespace {
 
-bool ExpDecayScheduler::ShouldShift(double from_score, double to_score) {
+bool ShouldAccept(double from_score, double to_score, long double temp,
+                  std::uniform_real_distribution<long double> &distr,
+                  std::mt19937 &rnd) {
     if (to_score < from_score) {
         return true;
     }
     long double delta =
         static_cast<long double>(to_score) - static_cast<long double>(from_score);
-    long double shift_probability = std::exp(-delta / temp_);
-    return distr_(rnd_) < shift_probability;
+    return distr(rnd) < std::exp(-delta / temp);
+}
+
+}  // namespace
+
+ExpDecayScheduler::ExpDecayScheduler(long double start, long double step,
+                                     long double min)
+    : temp_(start), step_(step), min_(min) {}
+
+bool ExpDecayScheduler::ShouldShift(double from_score, double to_score) {
+    return ShouldAccept(from_score, to_score, temp_, distr_, rnd_);
 }
 
 void ExpDecayScheduler::CoolDown() {
@@ -21,5 +30,38 @@ void ExpDecayScheduler::CoolDown() {
 }
 
 bool ExpDecayScheduler::IsFrozen() {
+    return temp_ <= min_;
+}
+
+LinearScheduler::LinearScheduler(long double start, long double decrement,
+                                 long double min)
+    : temp_(start), decrement_(decrement), min_(min) {}
+
+bool LinearScheduler::ShouldShift(double from_score, double to_score) {
+    return ShouldAccept(from_score, to_score, temp_, distr_, rnd_);
+}
+
+void LinearScheduler::CoolDown() {
+    temp_ -= decrement_;
+}
+
+bool LinearScheduler::IsFrozen() {
+    return temp_ <= min_;
+}
+
+ReciprocalScheduler::ReciprocalScheduler(long double start, long double rate,
+                                         long double min)
+    : temp_(start), start_(start), rate_(rate), min_(min) {}
+
+bool ReciprocalScheduler::ShouldShift(double from_score, double to_score) {
+    return ShouldAccept(from_score, to_score, temp_, distr_, rnd_);
+}
+
+void ReciprocalScheduler::CoolDown() {
+    ++iteration_;
+    temp_ = start_ / (1.0L + rate_ * iteration_);
+}
+
+bool ReciprocalScheduler::IsFrozen() {
     return temp_ <= min_;
 }
--- optlib/sched.h
+++ optlib/sched.h
@@ -2,6 +2,7 @@
 
 #include "optlib/public.h"
 
+#include <cstddef>
 #include <random>
 
 class ExpDecayScheduler : public Scheduler {
@@ -19,6 +20,44 @@ private:
   long double temp_;
   const long double step_;
   const long double min_;
+  std::uniform_real_distribution<long double> distr_{0.0L, 1.0L};
+  std::mt19937 rnd_;
+};
+
+class LinearScheduler : public Scheduler {
+public:
+  LinearScheduler(long double start, long double decrement, long double min);
+
+  bool ShouldShift(double from_score, double to_score) override;
+
+  void CoolDown() override;
+
+  bool IsFrozen() override;
+
+private:
+  long double temp_;
+  const long double decrement_;
+  const long double min_;
+  std::uniform_real_distribution<long double> distr_{0.0L, 1.0L};
+  std::mt19937 rnd_;
+};
+
+class ReciprocalScheduler : public Scheduler {
+public:
+  ReciprocalScheduler(long double start, long double rate, long double min);
+
+  bool ShouldShift(double from_score, double to_score) override;
+
+  void CoolDown() override;
+
+  bool IsFrozen() override;
+
+private:
+  long double temp_;
+  const long double start_;
+  const long double rate_;
+  const long double min_;
+  std::size_t iteration_ = 0;
   std::uniform_real_distribution<long double> distr_{0.0L, 1.0L};
   std::mt19937 rnd_;
 };
